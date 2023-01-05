1328c5c85177a940aa77309f607a27d80568bbab
isbecky27 <is.becky27@gmail.com>
2023-01-06 02:27:46 +0800

    remove unused code and code refactoring

--- detect.py
+++ detect.py
@@ -6,54 +6,58 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
+import numpy as np
 
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
+from utils.datasets import LoadStreams, LoadImages, letterbox
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
-global global_model
-global_model = None
-global global_modelc
-global_modelc = None
-global global_stride
-global_stride = None
+global model, stride, device, half, imgsz
+model, stride, device, half = None, None, None, None
+global names, colors
+names, colors = None, None
 
-def init_global_model(opt) -> None:
-    global global_model, global_stride
-    if global_model is not None:
+def init_model(opt) -> None:
+
+    global model, stride, device, half, imgsz
+
+    if model is not None:
         return
     
-    weights, view_img, save_txt, imgsz, trace = opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
-    save_img = not opt.nosave and not opt.source.endswith('.txt')  # save inference images
+    weights, imgsz, trace = opt.weights, opt.img_size, not opt.no_trace
     webcam = opt.source.isnumeric() or opt.source.endswith('.txt') or opt.source.lower().startswith(
         ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Initialize
     set_logging()
     device = select_device(opt.device)
     half = device.type != 'cpu'  # half precision only supported on CUDA
-    global_model = attempt_load(weights, map_location=device)  # load FP32 model
-    global_stride = int(global_model.stride.max())  # model stride
-    imgsz = check_img_size(imgsz, s=global_stride)  # check img_size
-    global_stride
+    
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+    
     if trace:
-        global_model = TracedModel(global_model, device, opt.img_size)
-    if half:
-        global_model.half()
+        model = TracedModel(model, device, opt.img_size)
 
-    global global_modelc
-    if global_modelc is not None:
-        return
-    # global_modelc = load_classifier(name='resnet101', n=2)  # initialize
-    # global_modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+    if half:
+        model.half()
 
     if device.type != 'cpu':
-        global_model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(global_model.parameters())))  # run once
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    
+    # Get names and colors
+    global names, colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
     
 def detect(opt, save_img=False):
-    global global_model
-    global global_modelc
+    global model
+    global modelc
     source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
     save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
@@ -63,7 +67,7 @@ def detect(opt, save_img=False):
     save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
-    if global_model is None:
+    if model is None:
         # Initialize
         set_logging()
         device = select_device(opt.device)
@@ -80,19 +84,19 @@ def detect(opt, save_img=False):
         if half:
             model.half()  # to FP16
     else:
-        model = global_model
+        model = model
         stride = global_stride
         device = select_device(opt.device)
         half = device.type != 'cpu'
     # Second-stage classifier
     classify = False
 
-    if global_modelc is None:
+    if modelc is None:
         if classify:
             modelc = load_classifier(name='resnet101', n=2)  # initialize
             modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
     else:
-        modelc = global_modelc
+        modelc = modelc
 
     # Set Dataloader
     vid_path, vid_writer = None, None
@@ -209,8 +213,80 @@ def detect(opt, save_img=False):
         #print(f"Results saved to {save_dir}{s}")
 
     # print(f'Done. ({time.time() - t0:.3f}s)')
-    return im0, bbox
+    return im0, bbox, time.time() - t0
 
+def detect_simple(opt, path, img0):
+
+    source, save_txt, save_dir = opt.source, opt.save_txt, opt.save_dir
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+   
+    p = Path(path)  # to Path
+    save_path = str(save_dir / p.name)  # img.jpg
+    txt_path = str(save_dir / 'labels' / p.stem) # + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+
+    # Padded resize
+    img = letterbox(img0, imgsz, stride=stride)[0]
+
+    # Convert
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+    img = np.ascontiguousarray(img)
+
+    img = torch.from_numpy(img).to(device)
+    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+
+    # Warmup
+    '''
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+    if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+        old_img_b = img.shape[0]
+        old_img_h = img.shape[2]
+        old_img_w = img.shape[3]
+        for i in range(3):
+            model(img, augment=opt.augment)[0]
+    '''
+
+    # Inference
+    t1 = time_synchronized()
+    with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+        pred = model(img, augment=opt.augment)[0]
+    t2 = time_synchronized()
+
+    # Apply NMS
+    pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+    t3 = time_synchronized()
+
+    bbox = []
+    im0 = img0
+    gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+    for i, det in enumerate(pred):  # detections per image
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+            # Write results
+            for *xyxy, conf, cls in reversed(det):
+                if save_txt:  # Write to file
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    line = (cls, *xywh, conf) if opt.save_conf else (cls, *xyxy)  # label format
+                    with open(txt_path + '.txt', 'a') as f:
+                        f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                
+                draw_bbox = True
+                if draw_bbox: # Draw Bounding Boxes
+                    label = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+                
+                if save_img:
+                    cv2.imwrite(save_path, im0)
+
+                bbox.append([i.item() for i in xyxy])
+        
+    # print(f'Done. ({time.time() - t0:.3f}s)')
+    return im0, bbox
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
